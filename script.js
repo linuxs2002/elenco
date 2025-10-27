6dc846c6032c8ea051b754aeb486807e7995c0f5
linuxs2002 <linux_s2002@yahoo.it>
2025-10-27 11:44:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener('DOMContentLoaded', function() {
+  const filtri = document.querySelectorAll('.filtro');
+  const cards = document.querySelectorAll('.card');
+  filtri.forEach(filtro => {
+    filtro.addEventListener('click', e => {
+      e.preventDefault();
+      filtri.forEach(f => f.classList.remove('attivo'));
+      filtro.classList.add('attivo');
+      const cat = filtro.dataset.cat;
+      cards.forEach(card => {
+        if(cat === 'tutte' || card.dataset.cat === cat) {
+          card.style.display = 'block';
+        } else {
+          card.style.display = 'none';
+        }
+      });
+    });
+  });
+});
